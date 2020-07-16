0e5c7bdb42977f6a566216a3f46cde6475a437ad
Aubyn92 <livstaubyn92@gmail.com>
2020-07-16 11:03:04 +1000

    Added delete button in posts page. Imported moment.js.

--- src/components/Posts.jsx
+++ src/components/Posts.jsx
@@ -1,4 +1,6 @@
 import React, { Component } from 'react'
+import { Link } from 'react-router-dom'
+import moment from 'moment'
 
 export default class Posts extends Component {
   state = { posts: [] }
@@ -9,6 +11,13 @@ export default class Posts extends Component {
     this.setState({ posts: posts })
   }
 
+  deletePost = async (id) => {
+    await fetch(`http://localhost:3000/posts/${id}`, {
+      method: "DELETE"
+    })
+    this.getPosts()
+  }
+
   renderPosts = () => {
     return this.state.posts.map((post, index) => {
       return (
@@ -16,6 +25,11 @@ export default class Posts extends Component {
           <h3>{post.title}</h3>
           <p>{post.description}</p>
           <span><a href={post.url} target="_blank" rel="noopener noreferrer">{post.url}</a></span>
+          <p>{moment(post.created_at).startOf('minute').fromNow()}</p>
+          <div className="edit-delete-container">
+            <Link to={`/posts/${post.id}/edit`}>Edit</Link>
+            <span onClick={() => this.deletePost(post.id)}>Delete</span>
+          </div>
           <hr/>
         </div>
       )
