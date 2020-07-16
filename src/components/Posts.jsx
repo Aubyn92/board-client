8f3dc1359fd47e09808473f60d727e5fa281d963
Aubyn92 <livstaubyn92@gmail.com>
2020-07-16 10:08:50 +1000

    added state, get and render to posts page

--- src/components/Posts.jsx
+++ src/components/Posts.jsx
@@ -1,10 +1,35 @@
 import React, { Component } from 'react'
 
 export default class Posts extends Component {
+  state = { posts: [] }
+
+  getPosts = async () => {
+    const response = await fetch("http://localhost:3000")
+    const posts = await response.json()
+    this.setState({ posts: posts })
+  }
+
+  renderPosts = () => {
+    return this.state.posts.map((post, index) => {
+      return (
+        <div key={index}>
+          <h3>{post.title}</h3>
+          <p>{post.description}</p>
+          <span><a href={post.url} target="_blank" rel="noopener noreferrer">{post.url}</a></span>
+          <hr/>
+        </div>
+      )
+    })
+  }
+
+  async componentDidMount() {
+    this.getPosts()
+  }
+
   render() {
     return (
       <div>
-        <p>Posts</p>
+        {this.renderPosts()}
       </div>
     )
   }
