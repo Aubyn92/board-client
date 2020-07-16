e28a06e6af31d67586554d4e9dca7a2375924b65
Aubyn92 <livstaubyn92@gmail.com>
2020-07-16 10:18:37 +1000

    added create form in NewPost component

--- src/components/NewPost.jsx
+++ src/components/NewPost.jsx
@@ -3,9 +3,19 @@ import React, { Component } from 'react'
 export default class NewPost extends Component {
   render() {
     return (
-      <div>
-        <p>NewPost</p>
+      <div className="container">
+        <form>
+          <label htmlFor="title">Title</label>
+          <input type="text" name="title" id="title" />
+          <label htmlFor="url">Url</label>
+          <input type="text" name="url" id="url" />
+          <label htmlFor="description">Description</label>
+          <textarea name="description" id="description"></textarea>
+          <input type="submit" value="Submit" />
+        </form>
       </div>
-    )
+    );
   }
 }
+
+
