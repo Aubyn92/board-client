1313f38356d86f554cc0c89100c048ef1500c8df
Aubyn92 <livstaubyn92@gmail.com>
2020-07-17 12:08:14 +1000

    changes to double ups on some routes in app.js

--- src/App.js
+++ src/App.js
@@ -1,47 +1,40 @@
-import React from 'react';
-import { Route, BrowserRouter } from 'react-router-dom';
-import EditPost from '/components/EditPost';
-import HomePage from './components/HomePage';
-import Login from './components/Login';
-import NewPost from './components/NewPost';
-import Posts from './components/Posts';
-import NavBar from './components/shared/NavBar';
-import ViewPost from './components/ViewPost';
-import SignUp from './components/SignUp';
-import UserProfile from './components/UserProfile';
-import CategoryPage from './components/CategoryPage';
-import NoMatch from './components/NoMatch';
-import SignUp from './components/Signup';
-import UserProfile from './components/UserProfile';
-import ViewPost from './components/ViewPost';
-import ProtectedRoute from './ProtectedRoute';
-import Secrets from './Secrets';
+import React from "react";
+import { Route, BrowserRouter } from "react-router-dom";
+import HomePage from "./components/HomePage";
+import NavBar from "./components/shared/NavBar";
+import Posts from "./components/Posts";
+import Login from "./components/Login";
+import SignUp from "./components/SignUp";
+import UserProfile from "./components/UserProfile";
+import CategoryPage from "./components/CategoryPage";
+import ViewPost from "./components/ViewPost";
+import EditPost from "/components/EditPost";
+import NewPost from "./components/NewPost";
+import NoMatch from "./components/NoMatch";
+import ProtectedRoute from "./ProtectedRoute";
+import Secrets from "./Secrets";
 
-
-function App() {
-  return (
-    <>
-    <NavBar />
-    <Switch>
-      <ProtectedRoute exact path="/secrets" component={Secrets} />
-      <Route exact path={"/"} component ={HomePage} />
-      <Route exact path={"/login"} component ={Login}/>
-      <Route exact path={"/newpost"} component ={NewPost} />
-      <Route exact path={"/posts/:id"} component ={Posts} />
-      <Route exact path="/posts/:id/edit" component={EditPost} />
-      <Route exact path="/posts/create" component={NewPost} />
-      <Route exact path="/posts/:id" component ={Posts} />
-      <Route exact path="/posts/:id/userprofile" component ={UserProfile} />
-      <Route exact path="/posts/categorypage" component = {CategoryPage}/>
-      <Route exact path="/login" component ={Login}/>
-      <Route exact path="/signup" component ={SignUp}/>
-      <Route exact path="/" component ={HomePage} />
-      <Route exact path="/posts/viewpost" component ={ViewPost} />
-      <Route component={NoMatch} />
-      </Switch>
-    </>
+class App extends React.Component {
+  render() {
+    return (
+      <>
+        <NavBar />
+        <Switch>
+          <ProtectedRoute exact path="/secrets" component={Secrets} />
+          <Route exact path={"/"} component={HomePage} />
+          <Route exact path={"/posts/:id"} component={Posts} />
+          <Route exact path={"/login"} component={Login} />
+          <Route exact path="/signup" component={SignUp} />
+          <Route exact path="/posts/:id/userprofile" component={UserProfile} />
+          <Route exact path="/posts/categorypage" component={CategoryPage} />
+          <Route exact path="/posts/viewpost" component={ViewPost} />
+          <Route exact path="/posts/:id/edit" component={EditPost} />
+          <Route exact path="/posts/create" component={NewPost} />
+          <Route component={NoMatch} />
+        </Switch>
+      </>
     );
   }
-  
-  export default App;
+}
 
+export default App;
--- src/components/NewPost.jsx
+++ src/components/NewPost.jsx
@@ -2,38 +2,39 @@ import React, { Component } from 'react'
 
 export default class NewPost extends Component {
   render() {
-
-    onInputChange = (event) => {
-      this.setState({
-        [event.target.id]: event.target.value
-      })
-    }
+  return (
+    <h1>Create new post</h1>
+    // onInputChange = (event) => {
+    //   this.setState({
+    //     [event.target.id]: event.target.value
+    //   })
+    // }
     
-    onFormSubmit = async (event) => {
-      event.preventDefault()
-      await fetch("http://localhost:3000/create", {
-        method: "POST",
-        headers: {
-          'Content-Type': "application/json"
-        },
-        body: JSON.stringify(this.state)
-      })
-      this.props.history.push("/posts")
-    }
+    // onFormSubmit = async (event) => {
+    //   event.preventDefault()
+    //   await fetch("http://localhost:3000/create", {
+    //     method: "POST",
+    //     headers: {
+    //       'Content-Type': "application/json"
+    //     },
+    //     body: JSON.stringify(this.state)
+    //   })
+    //   this.props.history.push("/posts")
+    // }
 
-    return (
-      <div className="container">
-        <form>
-          <label htmlFor="title">Title</label>
-          <input type="text" name="title" id="title" />
-          <label htmlFor="url">Url</label>
-          <input type="text" name="url" id="url" />
-          <label htmlFor="description">Description</label>
-          <textarea name="description" id="description"></textarea>
-          <input type="submit" value="Submit" />
-        </form>
-      </div>
-    );
+    // return (
+    //   <div className="container">
+    //     <form>
+    //       <label htmlFor="title">Title</label>
+    //       <input type="text" name="title" id="title" />
+    //       <label htmlFor="url">Url</label>
+    //       <input type="text" name="url" id="url" />
+    //       <label htmlFor="description">Description</label>
+    //       <textarea name="description" id="description"></textarea>
+    //       <input type="submit" value="Submit" />
+    //     </form>
+    //   </div>
+  )
   }
 }
 
