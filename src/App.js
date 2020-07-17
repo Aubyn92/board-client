b244bccd0bc23b05c7e325170ef3f132e2b0dd06
Aubyn92 <livstaubyn92@gmail.com>
2020-07-17 11:33:19 +1000

    fixing merge errors in edit, app and oackage.json

--- src/App.js
+++ src/App.js
@@ -6,16 +6,12 @@ import Login from './components/Login';
 import NewPost from './components/NewPost';
 import Posts from './components/Posts';
 import NavBar from './components/shared/NavBar';
-<<<<<<< HEAD
-import SignUp from './components/SignUp';
-=======
 import ViewPost from './components/ViewPost';
 import SignUp from './components/SignUp';
 import UserProfile from './components/UserProfile';
 import CategoryPage from './components/CategoryPage';
 import NoMatch from './components/NoMatch';
 import SignUp from './components/Signup';
->>>>>>> 6a6975a054a8bf6bb49e2602f196b395471a73ec
 import UserProfile from './components/UserProfile';
 import ViewPost from './components/ViewPost';
 import ProtectedRoute from './ProtectedRoute';
