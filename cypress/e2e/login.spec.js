78cca3d475c7a17ed6d6db045c6a8a8831877c5a
Aubyn92 <livstaubyn92@gmail.com>
2020-07-18 20:42:48 +1000

    Test for email and password inputs passed. Added beforeEach hook to describe block making the two tests independent.

--- cypress/e2e/login.spec.js
+++ cypress/e2e/login.spec.js
@@ -1,10 +1,30 @@
-describe("clicking on login", () => {
-    it("should go to the login page", () => {
+// describe("clicking on login", () => {
+//     it("should go to the login page", () => {
+//       cy.visit("/");
+//       cy.findByText(/Login/).click();
+//       cy.url().should("include", "/login");
+//     });
+//   it.only("should render email and password inputs", () => {
+//     cy.findByLabelText(/email/i).should("exist");
+//     cy.findByLabelText(/password/i).should("exist");
+//   });
+// });
+describe("when clicking on login from the homepage user", () => {
+    beforeEach(() => {
       cy.visit("/");
       cy.findByText(/Login/).click();
+    });
+ 
+    it("should go to the login page", () => {
       cy.url().should("include", "/login");
     });
+ 
+    it("should see email and password inputs", () => {
+      cy.findByLabelText(/email/i).should("exist");
+      cy.findByLabelText(/password/i).should("exist");
+    });
   });
+
 
 
  
