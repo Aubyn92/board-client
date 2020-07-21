4bf4efdb37e6abfc1081006a7ca72b9df8fde73c
Aubyn92 <livstaubyn92@gmail.com>
2020-07-21 16:46:59 +1000

    Added css for the home page and search bar.

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -3,10 +3,26 @@ import React, { Component } from 'react'
 export default class HomePage extends Component {
   render() {
     return (
-      <div>
-        <h1>Feel connected</h1>
-        <p>Share your interests, discover new ones</p>
-      </div>
+      <>
+     
+
+<section class="hero">
+  <div class="hero-body">
+    <div class="container">
+      <h1 class="title">
+        Feel connected
+      </h1>
+      <h2 class="subtitle">
+        Share your interests, discover new ones
+      </h2>
+      <div class="control">
+    <input class="input is-medium is-rounded" type="text" placeholder="Search new interest here" />
+</div>
+    </div>
+    
+  </div>
+</section>
+</>
     )
   }
 }
