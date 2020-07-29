f03f0d316f6b2693412ce42388a4069efcb1e85b
Aubyn92 <livstaubyn92@gmail.com>
2020-07-29 17:06:35 +1000

    Fixed hamburger menu in navbar yaygit add .git add .

--- src/components/shared/NavBar.jsx
+++ src/components/shared/NavBar.jsx
@@ -3,6 +3,7 @@ import { Link } from "react-router-dom";
 import { useHistory } from "react-router-dom";
 
 const NavBar = () => {
+  const [isActive, setisActive] = React.useState(false);
   const history = useHistory();
   if (localStorage.getItem("token") != null) {
     return (
@@ -17,6 +18,9 @@ const NavBar = () => {
               <Link to="/">BOARD</Link>
             </a>
             <a
+              onClick={() => {
+                setisActive(!isActive);
+              }}
               role="button"
               class="navbar-burger burger"
               aria-label="menu"
@@ -29,42 +33,45 @@ const NavBar = () => {
             </a>
           </div>
         </div>
-        
-        <div id="navbarBasicExample" class="navbar-menu">
+
+        <div
+          id="navbarBasicExample"
+          className={`navbar-menu ${isActive ? "is-active" : ""}`}
+        >
           <div class="navbar-end">
             <div class="navbar-item">
               <nav class="breadcrumb" aria-label="breadcrumbs">
                 <ul>
-                   <a class="navbar-item">
-                  <li>
-                    {" "}
-                    <Link to="/posts">Posts</Link>{" "}
-                  </li>
+                  <a class="navbar-item">
+                    <li>
+                      {" "}
+                      <Link to="/posts">Posts</Link>{" "}
+                    </li>
                   </a>
-                   <a class="navbar-item">
-                  <li>
-                    {" "}
-                    <Link to="/posts/create">Create Post</Link>{" "}
-                  </li>
+                  <a class="navbar-item">
+                    <li>
+                      {" "}
+                      <Link to="/posts/create">Create Post</Link>{" "}
+                    </li>
                   </a>
-                   <a class="navbar-item">
-                  <li>
-                    {" "}
-                    <Link to="/profile">Profile</Link>{" "}
-                  </li>
+                  <a class="navbar-item">
+                    <li>
+                      {" "}
+                      <Link to="/profile">Profile</Link>{" "}
+                    </li>
                   </a>
-                   <a class="navbar-item">
-                  <li>
-                    {" "}
-                    <span
-                      onClick={() => {
-                        localStorage.removeItem("token");
-                        history.push("/login");
-                      }}
-                    >
-                      Logout
-                    </span>{" "}
-                  </li>
+                  <a class="navbar-item">
+                    <li>
+                      {" "}
+                      <span
+                        onClick={() => {
+                          localStorage.removeItem("token");
+                          history.push("/login");
+                        }}
+                      >
+                        Logout
+                      </span>{" "}
+                    </li>
                   </a>
                 </ul>
               </nav>
@@ -85,30 +92,57 @@ const NavBar = () => {
             <a class="navbar-item">
               <Link to="/">BOARD</Link>
             </a>
+
+            <a
+              onClick={() => {
+                setisActive(!isActive);
+              }}
+              role="button"
+              class="navbar-burger burger"
+              aria-label="menu"
+              aria-expanded="false"
+              data-target="navbarBasicExample"
+            >
+              <span aria-hidden="true"></span>
+              <span aria-hidden="true"></span>
+              <span aria-hidden="true"></span>
+            </a>
           </div>
         </div>
-        <div class="navbar-end">
-          <div class="navbar-item">
-            <nav class="breadcrumb" aria-label="breadcrumbs">
-              <ul>
-                <li>
-                  <li>
-                    {" "}
-                    <Link to="/posts">Posts</Link>
-                  </li>
-                  <li>
-                    {" "}
-                    <Link to="/login" data-testid="login">
-                      Login
-                    </Link>
-                  </li>
-                  <li></li>{" "}
-                  <Link to="/sign-up" data-testid="sign-up">
-                    Sign Up
-                  </Link>
-                </li>
-              </ul>
-            </nav>
+
+        <div
+          id="navbarBasicExample"
+          className={`navbar-menu ${isActive ? "is-active" : ""}`}
+        >
+          <div class="navbar-end">
+            <div class="navbar-item">
+              <nav class="breadcrumb" aria-label="breadcrumbs">
+                <ul>
+                  <a class="navbar-item">
+                    <li>
+                      {" "}
+                      <Link to="/posts">Posts</Link>
+                    </li>
+                  </a>
+                  <a class="navbar-item">
+                    <li>
+                      {" "}
+                      <Link to="/login" data-testid="login">
+                        Login
+                      </Link>
+                    </li>
+                  </a>
+                  <a class="navbar-item">
+                    <li>
+                      {" "}
+                      <Link to="/sign-up" data-testid="sign-up">
+                        Sign Up
+                      </Link>
+                    </li>
+                  </a>
+                </ul>
+              </nav>
+            </div>
           </div>
         </div>
       </nav>
