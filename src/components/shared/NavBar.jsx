2e7685274f9d12ef5961b4ac98b19c18d4a0afc6
Aubyn92 <livstaubyn92@gmail.com>
2020-07-29 16:38:18 +1000

    Change to hamburger menu

--- src/components/shared/NavBar.jsx
+++ src/components/shared/NavBar.jsx
@@ -6,31 +6,54 @@ const NavBar = () => {
   const history = useHistory();
   if (localStorage.getItem("token") != null) {
     return (
-      
-        <nav class="navbar background-color is-light" role="navigation" aria-label="main navigation">
-          <div class="navbar-start">
-            <div class="navbar-brand">
+      <nav
+        class="navbar background-color is-light"
+        role="navigation"
+        aria-label="main navigation"
+      >
+        <div class="navbar-start">
+          <div class="navbar-brand">
             <a class="navbar-item">
               <Link to="/">BOARD</Link>
             </a>
-            </div>
+            <a
+              role="button"
+              class="navbar-burger burger"
+              aria-label="menu"
+              aria-expanded="false"
+              data-target="navbarBasicExample"
+            >
+              <span aria-hidden="true"></span>
+              <span aria-hidden="true"></span>
+              <span aria-hidden="true"></span>
+            </a>
           </div>
+        </div>
+        
+        <div id="navbarBasicExample" class="navbar-menu">
           <div class="navbar-end">
             <div class="navbar-item">
               <nav class="breadcrumb" aria-label="breadcrumbs">
                 <ul>
+                   <a class="navbar-item">
                   <li>
                     {" "}
                     <Link to="/posts">Posts</Link>{" "}
                   </li>
+                  </a>
+                   <a class="navbar-item">
                   <li>
                     {" "}
                     <Link to="/posts/create">Create Post</Link>{" "}
                   </li>
+                  </a>
+                   <a class="navbar-item">
                   <li>
                     {" "}
                     <Link to="/profile">Profile</Link>{" "}
                   </li>
+                  </a>
+                   <a class="navbar-item">
                   <li>
                     {" "}
                     <span
@@ -42,50 +65,53 @@ const NavBar = () => {
                       Logout
                     </span>{" "}
                   </li>
+                  </a>
                 </ul>
               </nav>
             </div>
           </div>
-        </nav>
-      
+        </div>
+      </nav>
     );
   } else {
     return (
-     
-        <nav class="navbar background-color" role="navigation" aria-label="main navigation">
-          <div class="navbar-start">
-            <div class="navbar-brand">
+      <nav
+        class="navbar background-color"
+        role="navigation"
+        aria-label="main navigation"
+      >
+        <div class="navbar-start">
+          <div class="navbar-brand">
             <a class="navbar-item">
               <Link to="/">BOARD</Link>
             </a>
-            </div>
           </div>
-          <div class="navbar-end">
-            <div class="navbar-item">
-              <nav class="breadcrumb" aria-label="breadcrumbs">
-                <ul>
+        </div>
+        <div class="navbar-end">
+          <div class="navbar-item">
+            <nav class="breadcrumb" aria-label="breadcrumbs">
+              <ul>
+                <li>
                   <li>
-                    <li>
-                      {" "}
-                      <Link to="/posts">Posts</Link>
-                    </li>
-                    <li>
-                      {" "}
-                      <Link to="/login" data-testid="login">
-                        Login
-                      </Link>
-                    </li>
-                    <li></li>{" "}
-                    <Link to="/sign-up" data-testid="sign-up">
-                      Sign Up
+                    {" "}
+                    <Link to="/posts">Posts</Link>
+                  </li>
+                  <li>
+                    {" "}
+                    <Link to="/login" data-testid="login">
+                      Login
                     </Link>
                   </li>
-                </ul>
-              </nav>
-            </div>
+                  <li></li>{" "}
+                  <Link to="/sign-up" data-testid="sign-up">
+                    Sign Up
+                  </Link>
+                </li>
+              </ul>
+            </nav>
           </div>
-        </nav>
-      
+        </div>
+      </nav>
     );
   }
 };
